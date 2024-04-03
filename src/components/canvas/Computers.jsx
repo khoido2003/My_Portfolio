0bdbcd92fbaa0cf4509e95d8cc3b442f011b74fd
Khoi Do <khoizpro31@gmail.com>
2024-04-03 22:56:19 +0700

    Create navbar and hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./baker/scene.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={1.5} groundColor="black" />
+      <pointLight intensity={2} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.14 : 0.24}
+        position={isMobile ? [0.2, -2.3, 0.1] : [0.3, -3.2, -0.3]}
+        rotation={[-0.01, 1.3, 0]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handdleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handdleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handdleMediaQueryChange);
+    };
+  }, []);
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
